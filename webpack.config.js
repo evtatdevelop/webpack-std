5ed265a1295bb90d86572d4387adfcaac2e2f0f3
Evgenii <evtatdevelop@gmail.com>
2020-12-15 16:24:59 +0700

    Lesson 1 Beginning settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 const delay = (timeout = 1000) =>
   new Promise(resolve => setTimeout(resolve,timeout));
 
+
+const cleanOptions = {
+  verbose: true,
+};
+const entry = './src/index.js';
+const output = {
+  filename: 'main.js',
+  path: path.resolve(__dirname, 'dist'),
+}
+
+
 // module.exports = async () => {
 //   //  пауза 1000 ms
 //   await delay();
-
 module.exports = () => {
 
   return {
+    entry: entry,
+    output: output,
     mode: 'none',
     devtool: false,
 
@@ -19,6 +33,7 @@ module.exports = () => {
         title: 'Learning WebPack!👀!',
         favicon: './static/favicon.ico',
       }),
+      new CleanWebpackPlugin(cleanOptions),
     ],
   };
 };

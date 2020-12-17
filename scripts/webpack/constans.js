78711d8dd74303a09c7ed91539901675095e9736
Evgenii <evtatdevelop@gmail.com>
2020-12-17 17:18:11 +0700

    Lesson 2 DevServer (hotreloading is already work without extra settings)

--- scripts/webpack/constans.js
+++ scripts/webpack/constans.js
@@ -6,3 +6,5 @@ console.log('-> project', PROJECT_ROOT);
 exports.PROJECT_ROOT = PROJECT_ROOT;
 exports.SOURCE_DIRECTORY = path.resolve(PROJECT_ROOT, './source');
 exports.BUILD_DIRECTORY = path.resolve(PROJECT_ROOT, './build');
+exports.HOST = 'localhost';
+exports.PORT = 3000;
